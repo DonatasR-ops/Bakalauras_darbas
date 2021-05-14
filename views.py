887a7d5e8a3fe6513a723916fcf4c7c84a749241
DonatasR-ops <57910891+DonatasR-ops@users.noreply.github.com>
2021-05-15 00:38:08 +0300

    fixes and features

--- views.py
+++ views.py
@@ -7,3 +7,13 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route('/gaze-real')
+def gaze_real():
+    return render_template("gaze_real.html", user=current_user)
+
+
+@views.route('/gaze-analyse')
+def gaze_analyse():
+    return render_template("gaze_analyse.html", user=current_user)
